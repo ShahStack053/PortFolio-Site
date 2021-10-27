088f7e05126c8a362145a736204abef0c86184d2
raza <shahhsoft@gmail.com>
2021-10-27 23:43:49 +0500

    wrapper

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,25 @@
+import "./intro.css"
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper"> </div>
+
+                    <h2 className="i-intro">Hi! My name is</h2>
+                    <h1 className="i-name"> Raza</h1>
+                    <div className="i-title"></div>
+                    <div className="i-title-wrapper"></div>
+                    <div className="i-title-item">Web Developer</div>
+                    <div className="i-title-item">UI/UX designer</div>
+                    <div className="i-title-item">Writer</div>
+                    <div className="i-title-item">Photographer</div>
+                    <div className="i-title-item">Content creater</div>
+                    
+                  </div>
+             <div className="i-right">right</div>
+        </div>
+    )
+}
+
+export default Intro
