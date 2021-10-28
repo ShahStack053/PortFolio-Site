eace2df9a45087c56f746854ba0ba4f94d842d4e
raza <shahhsoft@gmail.com>
2021-10-28 23:22:13 +0500

    Animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,21 +1,23 @@
 import "./intro.css"
-
 const Intro = () => {
     return (
         <div className="i">
             <div className="i-left">
-                <div className="i-left-wrapper"> </div>
+                <div className="i-left-wrapper"> 
 
                     <h2 className="i-intro">Hi! My name is</h2>
-                    <h1 className="i-name"> Raza</h1>
-                    <div className="i-title"></div>
-                    <div className="i-title-wrapper"></div>
+                    <h1 className="i-name"> Raza Shah</h1>
+                    <div className="i-title">
+                    <div className="i-title-wrapper">
                     <div className="i-title-item">Web Developer</div>
                     <div className="i-title-item">UI/UX designer</div>
                     <div className="i-title-item">Writer</div>
                     <div className="i-title-item">Photographer</div>
                     <div className="i-title-item">Content creater</div>
+                    </div>
+                    </div>
                     
+                    </div>
                   </div>
              <div className="i-right">right</div>
         </div>
